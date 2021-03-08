395c052e104582df7e4b78e91ae501f708a0e497
FlorenciaHQ <florenciahidalgoq@hotmail.com>
2021-03-08 10:11:18 -0300

    Tarea carpeta 07-JS-VI completa

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,23 +1,30 @@
 // Do not change any of the function names
 
 function mayuscula(nombre) {
+  return nombre[0].toUpperCase() + nombre.slice(1)
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
 }
 
 function invocarCallback(cb) {
+   cb()
   // Invoca al callback `cb`
   //Tu código:
 }
 
 function operacionMatematica(n1, n2, cb) {
+  return cb(n1,n2)
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
 }
 
 function sumarArray(numeros, cb) {
+  var algo= numeros.reduce(function(acc, elemento) {
+    return acc + elemento
+  })
+  console.log(cb(algo))
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
@@ -25,12 +32,23 @@ function sumarArray(numeros, cb) {
 }
 
 function forEach(array, cb) {
+  var algo= array.forEach(function(elemento){
+      cb(elemento)
+  })
+  
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
 }
 
 function map(array, cb) {
+  var algo= array.map(function(elemento) {
+   return cb(elemento)
+  })
+  var otro= []
+  if (array.length === algo.length){
+    return  otro= algo
+  }
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
@@ -38,6 +56,13 @@ function map(array, cb) {
 }
 
 function filter(array) {
+  var otro= []
+  for (var i= 0; i < array.length ; i++) {
+    if(array[i][0] === "a") {
+     otro.push(array[i])
+    }
+  }
+    return otro
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
